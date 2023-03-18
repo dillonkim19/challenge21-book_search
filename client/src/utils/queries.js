de675c335e3726cf845247f8bd4c8a6dcf972e22
Dillon Kim <dillonkim@Dillons-Air.attlocal.net>
2023-03-18 13:17:39 -0700

    add queries and mutations for frontend

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,17 +1,19 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_ME = gql`
-    query me($id: String, $username: String) {
-        _id
-        username
-        email
-        savedBooks {
-            authors
-            description
-            bookId
-            image
-            link
-            title
+    {
+        me{
+            _id
+            username
+            email
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
         }
     }
 `
